64e6d6e3b329cd3dcf2bf7af7fa57cd52c764951
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-08-18 04:02:23 +0200

    Refactor TextFormatter

--- src/com/github/antoinejt/jasc/util/TextFormatter.java
+++ src/com/github/antoinejt/jasc/util/TextFormatter.java
@@ -1,6 +1,10 @@
 package com.github.antoinejt.jasc.util;
 
+import java.io.PrintStream;
+
 public final class TextFormatter {
+    private static final PrintStream sysout = System.out;
+
     public static final class FormattedText {
         private final String formattedText;
 
@@ -13,16 +17,20 @@ public final class TextFormatter {
         } // It's not needed to call that, implicit call will be done when you print the object (?)
 
         public void print(){
-            System.out.print(formattedText);
+            sysout.print(formattedText);
         }
     }
 
     public static FormattedText formatLines(String prefix, String separator, String[] lines){
         StringBuilder stringBuilder = new StringBuilder();
         for (String line : lines){
-            stringBuilder.append(prefix).append(line).append(separator);
+            stringBuilder
+                    .append(prefix)
+                    .append(line)
+                    .append(separator);
         }
-        return new FormattedText(stringBuilder.toString());
+        String text = stringBuilder.toString();
+        return new FormattedText(text);
     }
 
     public static FormattedText formatLines(String prefix, String[] lines){
@@ -30,10 +38,11 @@ public final class TextFormatter {
     }
 
     public static void printLines(String... lines){
-        System.out.print(formatLines("", lines));
+        sysout.print(formatLines("", lines));
     }
 
     public static FormattedText listThings(String label, String... lines){
-        return new FormattedText(label + "\n" + formatLines("\t- ", "\n", lines));
+        FormattedText formattedText = formatLines("\t- ", "\n", lines);
+        return new FormattedText(label + "\n" + formattedText);
     }
 }
