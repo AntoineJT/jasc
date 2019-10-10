3dccf8ac7a41effe790164cf377524ebad56b4b8
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-10-10 19:11:29 +0200

    Remove unused CalculatorException

--- src/main/java/com/github/antoinejt/jasc/calculator/CalculatorException.java
+++
@@ -1,7 +0,0 @@
-package com.github.antoinejt.jasc.calculator;
-
-final class CalculatorException extends Exception { // TODO Maybe to remove and replace
-    CalculatorException(String msg) {
-        super(msg);
-    }
-}
