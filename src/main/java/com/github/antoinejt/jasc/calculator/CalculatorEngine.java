922b7202f169de6abf76e76ddb475cdd3f5d93c0
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-10-29 02:16:37 +0100

    Add MIT license to each source file, update lastUpdate

--- src/main/java/com/github/antoinejt/jasc/calculator/CalculatorEngine.java
+++ src/main/java/com/github/antoinejt/jasc/calculator/CalculatorEngine.java
@@ -3,6 +3,33 @@ package com.github.antoinejt.jasc.calculator;
 import java.util.List;
 import java.util.Stack;
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Antoine James Tournepiche
+ *
+ * This source file come from Just another Stack Calculator
+ * Repository : https://github.com/AntoineJT/jasc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 public class CalculatorEngine {
     private Stack<Float> stack = new Stack<>(); // TODO MrMicky says that it's better to use Deque instead of Stack
 
@@ -75,6 +102,7 @@ public class CalculatorEngine {
 
     private void reinjectOperandsIntoTheStack(float[] operands) {
         assert operands.length == 2;
+
         for (int i = 0; i < 2; i++) {
             stack.push(operands[1 - i]);
         }
@@ -82,6 +110,7 @@ public class CalculatorEngine {
 
     private float getOperationResult(OperationType operation, float[] operands) {
         assert operands.length >= 2;
+
         float a = operands[0];
         float b = operands[1];
 
