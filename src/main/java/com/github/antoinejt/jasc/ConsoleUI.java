b49e1667ab7eba6380813b814b672409a4670d2b
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-11-25 11:17:09 +0100

    :rotating_light: Remove unused imports

--- src/main/java/com/github/antoinejt/jasc/ConsoleUI.java
+++ src/main/java/com/github/antoinejt/jasc/ConsoleUI.java
@@ -33,10 +33,8 @@ import com.github.antoinejt.jasc.calculator.OperationType;
 import com.github.antoinejt.jasc.parser.MiniViewParser;
 import com.github.antoinejt.jasc.parser.View;
 import com.github.antoinejt.jasc.util.HashMapBuilder;
-import com.github.antoinejt.jasc.util.TextFormatter;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
